92e779424c6d196821c12e56a03f72bc0001c542
Simonow <slava.simonow@gmail.com>
2016-07-16 06:00:27 +0300

    start working on dynamic in js

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+window.addEventListener('load',function(){
+  var enterBtn = document.querySelector('#login-btn');
+  var loginModal = document.querySelector('#login');
+  var loginClose = document.querySelector('#login-close');
+  var loginSubmit = document.querySelector('#login-submit');
+  var mapBtn = document.querySelector('#howtogo');
+  var mapModal = document.querySelector('#modal-map');
+  var mapClose = document.querySelector('#map-close');
+/*  var mapBlock = document.createElement('div');*/
 
+  enterBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    loginModal.classList.add('show');
+  });
+  loginClose.addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    loginModal.classList.remove('show');
+  });
+  loginSubmit.addEventListener("click", function(e) {
+    e.stopPropagation();
+    loginModal.classList.remove('show');
+  });
+  mapBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    mapModal.classList.add('show-map');
+  });
+  mapClose.addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    mapModal.classList.remove('show-map');
+  });
+});
 
 
