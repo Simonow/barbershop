44e1518ebff2662bb82b584ba313bec7ec76ef2e
Simonow <slava.simonow@gmail.com>
2016-07-17 13:00:45 +0300

    wrote slider, update animation of buttons & menus

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ window.addEventListener('load',function(){
   var mapBtn = document.querySelector('#howtogo');
   var mapModal = document.querySelector('#modal-map');
   var mapClose = document.querySelector('#map-close');
-/*  var mapBlock = document.createElement('div');*/
+  var miniSliderImg = document.querySelector('.gallery-smpic');
+  var miniSliderNext = document.querySelector('#mini-gallery-next');
+  var miniSliderPrev = document.querySelector('#mini-gallery-prev');
+
 
   enterBtn.addEventListener("click", function(e) {
     e.preventDefault();
@@ -31,6 +34,31 @@ window.addEventListener('load',function(){
     e.preventDefault();
     e.stopPropagation();
     mapModal.classList.remove('show-map');
+  });
+
+  miniSliderPrev.addEventListener('click', function(e) {
+    var revSrc = miniSliderImg.src.split('').reverse().join('');
+    var imgPos = revSrc.charAt(4);
+    e.preventDefault();
+    e.stopPropagation();
+    if (imgPos < 4) {
+      miniSliderImg.src = revSrc.replace(imgPos, +imgPos+1).split('').reverse().join('');
+    }
+    else {
+      miniSliderImg.src = revSrc.replace(imgPos, 1).split('').reverse().join('');
+    }
+  });
+  miniSliderNext.addEventListener('click', function(e) {
+    var revSrc = miniSliderImg.src.split('').reverse().join('');
+    var imgPos = revSrc.charAt(4);
+    e.preventDefault();
+    e.stopPropagation();
+    if (imgPos > 1) {
+      miniSliderImg.src = revSrc.replace(imgPos, +imgPos-1).split('').reverse().join('');
+    }
+    else {
+      miniSliderImg.src = revSrc.replace(imgPos, 4).split('').reverse().join('');
+    }
   });
 });
 
